4f96de8277a7c7e9e083da89ff462bef6a732561
Oliver Zick <oliverzick@users.noreply.github.com>
2020-08-16 01:20:25 +0200

    Resolve #1: Enable match that matches always

---
+++ src/Delizious.Toolkit.Test/MatchSpec.cs
@@ -0,0 +1,19 @@
+namespace Delizious.Toolkit.Test
+{
+    using Xunit;
+
+    public sealed class MatchSpec
+    {
+        [Theory]
+        [InlineData(true, false)]
+        [InlineData(true, true)]
+        public void Always(bool expected, bool value)
+        {
+            var subject = Match.Always<bool>();
+
+            var actual = subject.Matches(value);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ src/Delizious.Toolkit/Match.cs
@@ -0,0 +1,72 @@
+﻿namespace Delizious
+{
+    /// <summary>
+    /// Provides static factory methods to create <see cref="Match{T}"/> instances.
+    /// </summary>
+    public static class Match
+    {
+        /// <summary>
+        /// Creates a <see cref="Match{T}"/> instance that always matches successfully regardless the value to match.
+        /// </summary>
+        /// <typeparam name="T">
+        /// The type of the value to match.
+        /// </typeparam>
+        /// <returns>
+        /// A new <see cref="Match{T}"/> instance that always matches successfully.
+        /// </returns>
+        public static Match<T> Always<T>()
+            => Match<T>.Create(PredefinedMatch<T>.Always());
+
+        private sealed class PredefinedMatch<T> : IMatch<T>
+        {
+            private readonly bool matches;
+
+            private PredefinedMatch(bool matches)
+            {
+                this.matches = matches;
+            }
+
+            public static PredefinedMatch<T> Always()
+                => new PredefinedMatch<T>(true);
+
+            public bool Matches(T value)
+                => this.matches;
+        }
+    }
+
+    /// <summary>
+    /// Represents a strongly typed match that provides a method to determine whether a value matches with.
+    /// </summary>
+    /// <typeparam name="T">
+    /// The type of the value to match.
+    /// </typeparam>
+    public sealed class Match<T> : IMatch<T>
+    {
+        private readonly IMatch<T> match;
+
+        private Match(IMatch<T> match)
+        {
+            this.match = match;
+        }
+
+        internal static Match<T> Create(IMatch<T> match)
+            => new Match<T>(match);
+
+        /// <summary>
+        /// Determines whether the specified <paramref name="value"/> successfully matches according to this match.
+        /// </summary>
+        /// <param name="value">
+        /// The value to match.
+        /// </param>
+        /// <returns>
+        /// <c>true</c> if <paramref name="value"/> successfully matches according to this match; otherwise, <c>false</c>.
+        /// </returns>
+        public bool Matches(T value)
+            => this.match.Matches(value);
+    }
+
+    internal interface IMatch<in T>
+    {
+        bool Matches(T value);
+    }
+}
