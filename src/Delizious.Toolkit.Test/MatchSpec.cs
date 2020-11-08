b84521801686513d430ace21a471cd89278a9929
Oliver Zick <oliverzick@users.noreply.github.com>
2020-11-08 19:08:29 +0100

    Reorder code by moving Same and NotSame matches before Equal match

--- src/Delizious.Toolkit.Test/MatchSpec.cs
+++ src/Delizious.Toolkit.Test/MatchSpec.cs
@@ -68,6 +68,66 @@ namespace Delizious
             }
         }
 
+        public sealed class Same
+        {
+            [Fact]
+            public void Throws_exception_when_reference_value_is_null()
+            {
+                Assert.Throws<ArgumentNullException>(() => Match.Same<object>(null!));
+            }
+
+            [Theory]
+            [MemberData(nameof(MatchesTheories))]
+            public void Matches(bool expected, object reference, object value)
+            {
+                var subject = Match.Same(reference);
+
+                var actual = subject.Matches(value);
+
+                Assert.Equal(expected, actual);
+            }
+
+            public static IEnumerable<object[]> MatchesTheories()
+            {
+                var instance1 = new object();
+                var instance2 = new object();
+
+                yield return DataTheory(true, instance1, instance1);
+                yield return DataTheory(true, instance2, instance2);
+                yield return DataTheory(false, instance1, instance2);
+            }
+        }
+
+        public sealed class NotSame
+        {
+            [Fact]
+            public void Throws_exception_when_reference_value_is_null()
+            {
+                Assert.Throws<ArgumentNullException>(() => Match.NotSame<object>(null!));
+            }
+
+            [Theory]
+            [MemberData(nameof(MatchesTheories))]
+            public void Matches(bool expected, object reference, object value)
+            {
+                var subject = Match.NotSame(reference);
+
+                var actual = subject.Matches(value);
+
+                Assert.Equal(expected, actual);
+            }
+
+            public static IEnumerable<object[]> MatchesTheories()
+            {
+                var instance1 = new object();
+                var instance2 = new object();
+
+                yield return DataTheory(false, instance1, instance1);
+                yield return DataTheory(false, instance2, instance2);
+                yield return DataTheory(true, instance1, instance2);
+            }
+        }
+
         public sealed class Equal
         {
             public sealed class EqualityComparer
@@ -211,66 +271,6 @@ namespace Delizious
                 yield return DataTheory(true, "Test", "TEST", StringComparer.Ordinal);
                 yield return DataTheory(false, "Test", "TEST", StringComparer.OrdinalIgnoreCase);
                 yield return DataTheory(false, "Test", "test", StringComparer.OrdinalIgnoreCase);
-            }
-        }
-
-        public sealed class Same
-        {
-            [Fact]
-            public void Throws_exception_when_reference_value_is_null()
-            {
-                Assert.Throws<ArgumentNullException>(() => Match.Same<object>(null!));
-            }
-
-            [Theory]
-            [MemberData(nameof(MatchesTheories))]
-            public void Matches(bool expected, object reference, object value)
-            {
-                var subject = Match.Same(reference);
-
-                var actual = subject.Matches(value);
-
-                Assert.Equal(expected, actual);
-            }
-
-            public static IEnumerable<object[]> MatchesTheories()
-            {
-                var instance1 = new object();
-                var instance2 = new object();
-
-                yield return DataTheory(true, instance1, instance1);
-                yield return DataTheory(true, instance2, instance2);
-                yield return DataTheory(false, instance1, instance2);
-            }
-        }
-
-        public sealed class NotSame
-        {
-            [Fact]
-            public void Throws_exception_when_reference_value_is_null()
-            {
-                Assert.Throws<ArgumentNullException>(() => Match.NotSame<object>(null!));
-            }
-
-            [Theory]
-            [MemberData(nameof(MatchesTheories))]
-            public void Matches(bool expected, object reference, object value)
-            {
-                var subject = Match.NotSame(reference);
-
-                var actual = subject.Matches(value);
-
-                Assert.Equal(expected, actual);
-            }
-
-            public static IEnumerable<object[]> MatchesTheories()
-            {
-                var instance1 = new object();
-                var instance2 = new object();
-
-                yield return DataTheory(false, instance1, instance1);
-                yield return DataTheory(false, instance2, instance2);
-                yield return DataTheory(true, instance1, instance2);
             }
         }
 
